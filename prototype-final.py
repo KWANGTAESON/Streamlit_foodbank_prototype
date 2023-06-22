c448fe1a2b6f7e449f74501c37d09e9f500dfd27
KWANGTAESON <117577204+KWANGTAESON@users.noreply.github.com>
2023-06-22 17:20:39 +0900

    Update prototype-final.py

--- prototype-final.py
+++ prototype-final.py
@@ -20,7 +20,7 @@ import matplotlib.font_manager as fm
 
 # 시각화 한글폰트 설정
 plt.rc('font', family='Malgun Gothic')
-sns.set(font=font_prop, 
+sns.set(font="Malgun Gothic", 
 rc={"axes.unicode_minus":False}, # 마이너스 부호 깨짐 현상 해결
 style='darkgrid')
 
