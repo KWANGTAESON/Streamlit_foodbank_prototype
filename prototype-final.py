4c987cf2d6a0fa8bcbd6571c55ebc89b6d5b7701
KWANGTAESON <117577204+KWANGTAESON@users.noreply.github.com>
2023-06-22 16:04:08 +0900

    Update prototype-final.py

--- prototype-final.py
+++ prototype-final.py
@@ -17,14 +17,14 @@ import re
 import joblib
 import platform
 from matplotlib import font_manager, rc
-plt.rcParams['axes.unicode_minus'] = False
+
+# 시각화 한글폰트 설정
+plt.rc('font', family='Malgun Gothic')
+sns.set(font="Malgun Gothic",#"NanumGothicCoding", 
+rc={"axes.unicode_minus":False}, # 마이너스 부호 깨짐 현상 해결
+style='darkgrid')
 if platform.system() == 'Linux':
     rc('font', family='NanumGothic')
-# 시각화 한글폰트 설정
-# plt.rc('font', family='Malgun Gothic')
-# sns.set(font="Malgun Gothic",#"NanumGothicCoding", 
-# rc={"axes.unicode_minus":False}, # 마이너스 부호 깨짐 현상 해결
-# style='darkgrid')
 
 
 
