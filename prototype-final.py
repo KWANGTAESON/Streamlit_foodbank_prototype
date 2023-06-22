56d00f732e711838def358f1bd65d6751a365292
KWANGTAESON <117577204+KWANGTAESON@users.noreply.github.com>
2023-06-22 16:30:32 +0900

    Update prototype-final.py

--- prototype-final.py
+++ prototype-final.py
@@ -28,7 +28,6 @@ style='darkgrid')
 font_path = "malgun.ttf"
 
 # CSS 스타일시트에 폰트 로드
-@st.cache(allow_output_mutation=True)
 def load_font(font_path):
     with open(font_path, "rb") as f:
         font_data = f.read()
